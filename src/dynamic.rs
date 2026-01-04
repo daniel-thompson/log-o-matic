08fa2019edebb75119ae49bdfdbf2cb78479d374
Daniel Thompson <daniel@redfelineninja.org.uk>
2026-01-04 19:35:43 +0000

    dynamic: Improve time management

--- src/dynamic.rs
+++ src/dynamic.rs
@@ -1,12 +1,12 @@
+use std::time::Duration;
 use std::{
     cmp::{max, min},
     error::Error,
     net::IpAddr,
     thread,
-    time::Duration,
 };
 
-use chrono::Timelike;
+use chrono::{DateTime, Local, MappedLocalTime, NaiveTime, Timelike};
 use clap::Parser;
 use log_o_matic::*;
 use rust_tuyapi::{Payload, TuyaDevice};
@@ -35,29 +35,41 @@ fn update_avanti(ip_addr: IpAddr, local_key: &str, flame: &Flame) -> Result<(), 
     Ok(())
 }
 
+fn get_next_occurrence(current_dt: &DateTime<Local>, target_time: NaiveTime) -> DateTime<Local> {
+    let candidate = current_dt
+        .date_naive()
+        .and_time(target_time)
+        .and_local_timezone(Local);
+
+    let target_dt = match candidate {
+        MappedLocalTime::Single(dt) => dt,
+        MappedLocalTime::Ambiguous(dt1, dt2) => dt1,
+        MappedLocalTime::None => {
+            panic!("Local time does not exist (gap)");
+        }
+    };
+
+    if &target_dt <= current_dt {
+        target_dt + chrono::Duration::days(1)
+    } else {
+        target_dt
+    }
+}
+
 pub fn main(ip_addr: IpAddr, local_key: &str, args: Command) -> Result<(), Box<dyn Error>> {
-    let start = chrono::Local::now() - chrono::Duration::minutes(args.age);
-    let bed_time = chrono::NaiveTime::from_hms_opt(22, 45, 0).unwrap();
     let mut now = chrono::Local::now();
+    now = now
+        - chrono::Duration::seconds(now.second() as i64)
+        - chrono::Duration::nanoseconds(now.nanosecond() as i64);
+
+    let start = now - chrono::Duration::minutes(args.age);
+    let bed_time = get_next_occurrence(&start, chrono::NaiveTime::from_hms_opt(22, 45, 0).unwrap());
 
     loop {
-        if args.dry_run {
-            now += chrono::Duration::minutes(2);
-        } else {
-            now = chrono::Local::now();
-        }
-        let target_today = now
-            .date_naive()
-            .and_time(bed_time)
-            .and_local_timezone(chrono::Local)
-            .unwrap();
         let age = now.signed_duration_since(start).num_minutes();
-        let remaining = target_today.signed_duration_since(now).num_minutes();
+        let remaining = bed_time.signed_duration_since(now).num_minutes();
         let offset = now.minute();
-
-        let mut time_since_log = std::cmp::min(age, offset as i64);
-        // TODO: this doesn't work... we need the threshold to be the minutes
-        //       value for "bed time"
+        let time_since_log = std::cmp::min(age, offset as i64);
 
         if remaining <= -50 {
             break;
@@ -94,13 +106,18 @@ pub fn main(ip_addr: IpAddr, local_key: &str, args: Command) -> Result<(), Box<d
 
         let flame = Flame::summon_fire(min(age * 2, 100) as u8, fuel_level, draw);
 
+        let then = now + chrono::Duration::minutes(1);
         if !args.dry_run {
             let e = update_avanti(ip_addr, local_key, &flame);
             match e {
-                Ok(_) => thread::sleep(Duration::from_secs(120)),
+                Ok(_) => {
+                    let sleep = then.signed_duration_since(chrono::Local::now());
+                    thread::sleep(sleep.to_std().unwrap_or(std::time::Duration::from_secs(60)));
+                }
                 Err(e) => println!("{e}"),
             };
         }
+        now = then;
     }
 
     Ok(())
