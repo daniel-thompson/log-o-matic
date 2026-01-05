30670c258f0f1cb7fbf95ab87eb55af6698ae7bb
Daniel Thompson <daniel@redfelineninja.org.uk>
2026-01-05 22:45:05 +0000

    dynamic: Automatically turn on and parameterize bed_time

--- src/dynamic.rs
+++ src/dynamic.rs
@@ -1,4 +1,4 @@
-use std::time::Duration;
+use std::str::FromStr;
 use std::{
     cmp::{max, min},
     error::Error,
@@ -10,6 +10,11 @@ use chrono::{DateTime, Local, MappedLocalTime, NaiveTime, Timelike};
 use clap::Parser;
 use log_o_matic::*;
 use rust_tuyapi::{Payload, TuyaDevice};
+use serde_json::json;
+
+fn parse_naive_time(s: &str) -> Result<NaiveTime, String> {
+    NaiveTime::from_str(s).map_err(|e| format!("Invalid time format: {}", e))
+}
 
 #[derive(Debug, Parser)]
 pub struct Command {
@@ -20,16 +25,35 @@ pub struct Command {
     /// Specify how long (in minutes) the fire has been lit for
     #[arg(long, default_value_t = 0)]
     age: i64,
+
+    /// The time the first should go out, in HH:MM format
+    #[arg(short, long, default_value = "22:45", value_parser = parse_naive_time)]
+    bed_time: NaiveTime,
+}
+
+fn flame_on(ip_addr: IpAddr, local_key: &str) -> Result<(), Box<dyn Error>> {
+    let cmd = json!({
+        "dps": {
+            "10": true
+        }
+    });
+    let payload = Payload::String(cmd.to_string());
+
+    let tuya_device = TuyaDevice::create("ver3.3", Some(local_key), ip_addr)?;
+    tuya_device.set_new(payload, 0)?;
+
+    Ok(())
 }
 
 fn update_avanti(ip_addr: IpAddr, local_key: &str, flame: &Flame) -> Result<(), Box<dyn Error>> {
+    let cmd = json!({
+        "dps": {
+            "104": flame.to_base64(),
+        }
+    });
+    let payload = Payload::String(cmd.to_string());
+
     let tuya_device = TuyaDevice::create("ver3.3", Some(local_key), ip_addr)?;
-
-    let payload = Payload::String(format!(
-        "{{\"dps\":{{\"104\": \"{}\"}}}}",
-        flame.to_base64()
-    ));
-
     tuya_device.set_new(payload, 0)?;
 
     Ok(())
@@ -43,7 +67,7 @@ fn get_next_occurrence(current_dt: &DateTime<Local>, target_time: NaiveTime) -> 
 
     let target_dt = match candidate {
         MappedLocalTime::Single(dt) => dt,
-        MappedLocalTime::Ambiguous(dt1, dt2) => dt1,
+        MappedLocalTime::Ambiguous(dt1, _dt2) => dt1,
         MappedLocalTime::None => {
             panic!("Local time does not exist (gap)");
         }
@@ -57,13 +81,15 @@ fn get_next_occurrence(current_dt: &DateTime<Local>, target_time: NaiveTime) -> 
 }
 
 pub fn main(ip_addr: IpAddr, local_key: &str, args: Command) -> Result<(), Box<dyn Error>> {
+    let mut first_loop = true;
+
     let mut now = chrono::Local::now();
     now = now
         - chrono::Duration::seconds(now.second() as i64)
         - chrono::Duration::nanoseconds(now.nanosecond() as i64);
 
     let start = now - chrono::Duration::minutes(args.age);
-    let bed_time = get_next_occurrence(&start, chrono::NaiveTime::from_hms_opt(22, 45, 0).unwrap());
+    let bed_time = get_next_occurrence(&start, args.bed_time.clone());
 
     loop {
         let age = now.signed_duration_since(start).num_minutes();
@@ -111,6 +137,10 @@ pub fn main(ip_addr: IpAddr, local_key: &str, args: Command) -> Result<(), Box<d
             let e = update_avanti(ip_addr, local_key, &flame);
             match e {
                 Ok(_) => {
+                    if first_loop {
+                        let _ = flame_on(ip_addr, local_key);
+                        first_loop = false;
+                    }
                     let sleep = then.signed_duration_since(chrono::Local::now());
                     thread::sleep(sleep.to_std().unwrap_or(std::time::Duration::from_secs(60)));
                 }
